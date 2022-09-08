c46d6b5f1b6e737e70a6bc60e443a66270e45dd1
Olga Pushkina <emmanuilaklein@gmail.com>
2022-09-08 14:03:07 +0300

    All in donw

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        System.out.println((int)c);
+        System.out.println((int)h);
+        System.out.println((int)a);
+        System.out.println((int)r);
+
+        System.out.println(99);
+        System.out.println(104);
+        System.out.println(97);
+        System.out.println(114);
+
     }
 }
